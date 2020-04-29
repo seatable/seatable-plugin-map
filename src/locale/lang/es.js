5ff30531c46ca1614ec23e437adfa2e47d783815
love-YGX-and-D <1578236889@qq.com>
2020-04-29 14:25:14 +0800

    feat: add es

---
+++ src/locale/lang/es.js
@@ -0,0 +1,10 @@
+const es = {
+  "Map_plugin": "Complemento de mapa",
+  "You_have_not_configured_the_map_application_please_contact_the_administrator_for_related_configuration": "No ha configurado la aplicación de mapas, póngase en contacto con el administrador para la configuración relacionada.",
+  "Table": "Forma",
+  "View": "Vista",
+  "Address_field": "Campo de dirección",
+  "Settings": "Configurar"
+};
+
+export default es; 
