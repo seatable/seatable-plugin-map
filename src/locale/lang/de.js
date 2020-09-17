26bf64314b600547564133327f476ba4da26d383
shanshuirenjia <978987373@qq.com>
2020-09-17 11:58:59 +0800

    update translate

--- src/locale/lang/de.js
+++ src/locale/lang/de.js
@@ -1,12 +1,12 @@
 const de = {
-  "Map_plugin": "Map plugin",
-  "You_have_not_configured_the_map_application_please_contact_the_administrator_for_related_configuration": "You have not configured the map application, please contact the administrator for related configuration.",
-  "Table": "Table",
-  "View": "View",
-  "Address_field": "Address field",
-  "Settings": "Settings",
-  "Your_Google_Maps_key_has_exceeded_quota": "Your Google Maps key has exceeded quota.",
-  "address_not_be_found": "{address} not be found."
+  "Map_plugin": "Karte",
+  "You_have_not_configured_the_map_application_please_contact_the_administrator_for_related_configuration": "Sie haben das Map Plugin nicht richtig konfiguriert. Kontaktieren Sie den Administrator.",
+  "Table": "Tabelle",
+  "View": "Ansicht",
+  "Address_field": "Adresse",
+  "Settings": "Einstellungen",
+  "Your_Google_Maps_key_has_exceeded_quota": "Ihr Google Maps Schlüssel hat seine Quota ausgeschöpft.",
+  "address_not_be_found": "{address} kannt nicht gefunden werden."
 };
 
 export default de;
--- src/locale/lang/fr.js
+++ src/locale/lang/fr.js
@@ -1,12 +1,12 @@
 const fr = {
-  "Map_plugin": "Map plugin",
-  "You_have_not_configured_the_map_application_please_contact_the_administrator_for_related_configuration": "You have not configured the map application, please contact the administrator for related configuration.",
-  "Table": "Table",
-  "View": "View",
-  "Address_field": "Address field",
-  "Settings": "Settings",
-  "Your_Google_Maps_key_has_exceeded_quota": "Your Google Maps key has exceeded quota.",
-  "address_not_be_found": "{address} not be found."
+  "Map_plugin": "Plan",
+  "You_have_not_configured_the_map_application_please_contact_the_administrator_for_related_configuration": "Vous n'avez pas configuré le plugin plan. Contactez l'administrateur.",
+  "Table": "Tableau",
+  "View": "Vue",
+  "Address_field": "Adresse",
+  "Settings": "Paramètres",
+  "Your_Google_Maps_key_has_exceeded_quota": "Votre clé Google Maps a épuisé son quota.",
+  "address_not_be_found": "{address} ne peut pas être trouvé."
 };
 
 export default fr;
