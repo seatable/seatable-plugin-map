33804977df8c28bbcd8eaca73aada3dcb05024a9
shanshuirenjia <978987373@qq.com>
2020-09-18 16:06:33 +0800

    update translate

--- src/locale/lang/de.js
+++ src/locale/lang/de.js
@@ -1,12 +1,14 @@
 const de = {
-  "Map_plugin": "Karte",
+  "Map_plugin": "Map",
   "You_have_not_configured_the_map_application_please_contact_the_administrator_for_related_configuration": "Sie haben das Map Plugin nicht richtig konfiguriert. Kontaktieren Sie den Administrator.",
   "Table": "Tabelle",
   "View": "Ansicht",
   "Address_field": "Adresse",
   "Settings": "Einstellungen",
   "Your_Google_Maps_key_has_exceeded_quota": "Ihr Google Maps Schlüssel hat seine Quota ausgeschöpft.",
-  "address_not_be_found": "{address} kannt nicht gefunden werden."
+  "address_not_be_found": "{address} kannt nicht gefunden werden.",
+  "Not_used": "Not used",
+  "Color_field": "Color filed"
 };
 
 export default de;
