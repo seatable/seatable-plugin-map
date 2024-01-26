722311f9414f18c58412ee3371f962bbeaee2273
张玉鑫 <zhangyuxin9102@163.com>
2024-01-26 17:56:44 +0800

    change panel position

--- src/setting.js
+++ src/setting.js
@@ -7,7 +7,7 @@ let config = {
   server: 'http://127.0.0.1:80',
   workspaceID: '1',
   dtableName: 'test-1',
-  lang: 'zh-cn'
+  lang: 'zh-CN'
 };
 
 /** (2/5) load local development settings ./setting.local.js (if exist) */
